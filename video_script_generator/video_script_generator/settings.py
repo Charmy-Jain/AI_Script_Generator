c0ef92f95a65d52d1d0eb7689305114792cfe2a5
Charmy Jain <charmyjain1@gmail.com>
2025-02-12 13:57:46 +0530

    final commit

--- video_script_generator/video_script_generator/settings.py
+++ video_script_generator/video_script_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-g6*0hv8e55@^hsf-f9q3qy17&t5k#1n*5(0d1u4p$qy@c!7qy#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'script_generator/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
