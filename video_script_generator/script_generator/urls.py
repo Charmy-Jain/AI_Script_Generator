69cdadc6dc132cc83033e522a1d2bae1ce6e2231
Charmy Jain <charmyjain1@gmail.com>
2025-02-17 01:09:05 +0530

    Initial commit

--- video_script_generator/script_generator/urls.py
+++ video_script_generator/script_generator/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import home, generate_script, upload_file, fetch_metadata
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("",home, name="home"),
@@ -7,3 +9,4 @@ urlpatterns = [
     path("upload_file/", upload_file, name="upload_file"),
     path("fetch_metadata/", fetch_metadata, name="fetch_metadata"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
